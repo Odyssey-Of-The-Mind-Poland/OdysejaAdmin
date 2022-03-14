ae464c5bff2862e2715cc0efb1692595f5b56723
grzybek <grzybol.k@gmail.com>
2022-03-14 19:22:12 +0100

    fixed deploy

--- OdysejaAdmin/deploy.sh
+++ OdysejaAdmin/deploy.sh
@@ -1,4 +1,4 @@
 docker build -t grzybek/odyseja-admin .
-ssh grzybek docker stop $(docker ps -q --filter ancestor=grzybek/odyseja-admin )
+ssh grzybek docker stop $(ssh grzybek docker ps -q --filter ancestor=grzybek/odyseja-admin )
 docker save grzybek/odyseja-admin | bzip2 | pv | ssh grzybek docker load
 ssh grzybek docker run --net=host -d grzybek/odyseja-admin
